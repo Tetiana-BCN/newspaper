38e13df840cbdf205ff6b984b0b9c86e4ac2c945
marianamarinflor622 <marianamarinflor@gmail.com>
2025-04-09 14:46:29 +0200

    PDA-2-FEAT-articule

--- src/main/java/com/newspaper/newspaper/model/Article.java
+++ src/main/java/com/newspaper/newspaper/model/Article.java
@@ -1,5 +1,86 @@
 package com.newspaper.newspaper.model;
 
+import java.time.LocalDate;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class Article {
+    @Id
+    @jakarta.persistence.GeneratedValue(strategy = GenerationType.IDENTITY)    
+    private Long id;
+
+    private String title;   
+    private String content;
+    private String category;
+    private LocalDate publicationDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;  
+
+    public Article() {
+
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCategory() {
+        return this.category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public LocalDate getPublicationDate() {
+        return this.publicationDate;
+    }
+
+    public void setPublicationDate(LocalDate publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public User getUser() {
+        return this.user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+
+
+
+    
+
+    
     
 }
