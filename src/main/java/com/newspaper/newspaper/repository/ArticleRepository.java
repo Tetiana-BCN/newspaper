78d9c1e792a555933d551006adbd7e68d1028a1a
Tetiana-BCN <tetiana.v.a@gmail.com>
2025-04-09 15:23:36 +0200

    PDA-24-FEAT Repository: ArticleRepository.java

--- src/main/java/com/newspaper/newspaper/repository/ArticleRepository.java
+++ src/main/java/com/newspaper/newspaper/repository/ArticleRepository.java
@@ -1,5 +1,9 @@
 package com.newspaper.newspaper.repository;
 
-public class ArticleRepository {
-    
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.newspaper.newspaper.model.Article;
+
+@Repository
+public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
