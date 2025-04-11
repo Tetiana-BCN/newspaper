69703a9180c2b997fb57eef42dfb9d852ab7a86b
edmimi <mirisaor02@gmail.com>
2025-04-11 16:11:12 +0200

    PDA-33-FIX ArticleControler and UserControler

--- src/main/java/com/newspaper/newspaper/controller/ArticleController.java
+++ src/main/java/com/newspaper/newspaper/controller/ArticleController.java
@@ -1,5 +1,30 @@
 package com.newspaper.newspaper.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.newspaper.newspaper.model.Article;
+import com.newspaper.newspaper.service.ArticleService;
+
+@RestController
+@RequestMapping("/api/v1/article")
+
 public class ArticleController {
-    
+
+    private final ArticleService articleService;
+
+    public ArticleController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Article> createArticle(@RequestBody Article article) {
+        Article newArticle = articleService.createArticle(article);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newArticle);
+    }
 }
+
--- src/main/java/com/newspaper/newspaper/controller/UserController.java
+++ src/main/java/com/newspaper/newspaper/controller/UserController.java
@@ -1,5 +1,29 @@
 package com.newspaper.newspaper.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.newspaper.newspaper.model.User;
+import com.newspaper.newspaper.service.UserService;
+
+@RestController
+@RequestMapping("/api/v1/users")
+
 public class UserController {
-    
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        return userService.createUser(user);
+
+    }
+    // nos faltaría agregar los endpoints que necesitemos
 }
