9daf7df724f2b0193303fd80ae1e20afed6fd110
marianamarinflor622 <marianamarinflor@gmail.com>
2025-04-11 13:03:39 +0200

    PDA-25-FIX-User.java

--- src/main/java/com/newspaper/newspaper/model/User.java
+++ src/main/java/com/newspaper/newspaper/model/User.java
@@ -1,32 +1,49 @@
 package com.newspaper.newspaper.model;
+
 import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
-
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+    @NotEmpty(message = "Name cannot be empty")
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
     private String name;
+    @NotEmpty(message = "Email cannot be empty")
+    @Size(min = 5, max = 100, message = "Email must be between 5 and 100 characters")
     private String email;
 
-    @OneToMany(mappedBy = "user")
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Article> articles;
+
+    public List<Article> getArticles() {
+        return this.articles;
+    }
+
+    public void setArticles(List<Article> articles) {
+        this.articles = articles;
+    }
 
     public User() {
     }
-    public User( Long id, String name, String email) { 
+
+    public User(Long id, String name, String email) {
         this.id = id;
         this.name = name;
         this.email = email;
     }
-    
 
     public Long getId() {
         return this.id;
@@ -51,5 +68,5 @@ public class User {
     public void setEmail(String email) {
         this.email = email;
     }
-    
+
 }
