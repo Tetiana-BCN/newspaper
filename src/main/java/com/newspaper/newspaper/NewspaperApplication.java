5492b5917ad60f3082483f840a02e236345bf54c
SiGu <miskybox@gmail.com>
2025-04-22 11:44:27 +0200

    FEAT-README.md

--- src/main/java/com/newspaper/newspaper/NewspaperApplication.java
+++ src/main/java/com/newspaper/newspaper/NewspaperApplication.java
@@ -11,30 +11,3 @@ public class NewspaperApplication {
 
 	}
 }
-/*
- * 
- * package com.newspaper.newspaper;
- * 
- * import java.util.Properties;
- * 
- * import org.springframework.boot.SpringApplication;
- * import org.springframework.boot.autoconfigure.SpringBootApplication;
- * 
- * import io.github.cdimascio.dotenv.Dotenv;
- * 
- * @SpringBootApplication
- * public class NewspaperApplication {
- * 
- * public static void main(String[] args) {
- * // Cargar .env
- * Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
- * Properties props = new Properties();
- * dotenv.entries().forEach(entry -> props.setProperty(entry.getKey(),
- * entry.getValue()));
- * 
- * SpringApplication app = new SpringApplication(NewspaperApplication.class);
- * app.setDefaultProperties(props);
- * app.run(args);
- * }
- * }
- */
